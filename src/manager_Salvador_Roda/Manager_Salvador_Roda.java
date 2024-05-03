89c7423b2f5e0e39f36675a61f4053f38869b9c7
salroal <salroal@hotmail.com>
2024-05-03 17:37:48 +0200

    El manager ha creado el proyecto base

---
+++ src/manager_Salvador_Roda/Manager_Salvador_Roda.java
@@ -0,0 +1,12 @@
+package manager_Salvador_Roda;
+
+public class Manager_Salvador_Roda {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+				System.out.println("Hola me llamo Salvador Roda y soy el manager");
+
+	}
+
+}
